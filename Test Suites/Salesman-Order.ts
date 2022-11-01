83d1cc7a00c574088c5df592e896f5ff779078ca
user <user@DESKTOP-FTHSTCS>
2022-11-01 11:03:25 +0800

    test suit salesman  - order

--- Test Suites/Salesman-Order.ts
+++ Test Suites/Salesman-Order.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>43e3226c-9401-4e87-981e-8fbb75a797d2</testSuiteGuid>
+   <testCaseLink>
+      <guid>857525a0-ea2a-4c36-a700-ac933c071ace</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Order</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
